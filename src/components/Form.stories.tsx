7d03d707a9f778af756a8264dab0a07cecc87145
keisuke watanabe <kskwtnb93@gmail.com>
2024-02-18 17:29:58 +0900

    コメント追加

--- src/components/Form.stories.tsx
+++ src/components/Form.stories.tsx
@@ -20,6 +20,10 @@ export const Testing: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement); // react testing libraryのscreenと似ているもの
 
+    // プロジェクトによるが、基本的にテストはJestだけでOK
+    // → Jest一箇所にテストをまとめた方が良い
+    // → StoryBookのインタラクションテストではモックが使えない（現状）など機能が不足している
+
     // input要素の取得
     const input = canvas.getByRole("textbox");
     // 初期状態が空であることをテスト
